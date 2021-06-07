878d1996ff18ac0efe0435a17f0b70951295c0bd
Chris Essoe <chrisessoe@gmail.com>
2021-06-07 12:54:03 -0700

    Animated Dice Rolls

--- dice/src/RollDice.js
+++ dice/src/RollDice.js
@@ -8,7 +8,7 @@ class RollDice extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = { die1: "one", die2: "two" };
+    this.state = { die1: "one", die2: "two", rolling: false };
     this.roll = this.roll.bind(this);
   }
   roll() {
@@ -16,15 +16,22 @@ class RollDice extends Component {
       this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
     const newDie2 =
       this.props.sides[Math.floor(Math.random() * this.props.sides.length)];
-    this.setState({ die1: newDie1, die2: newDie2 });
+    this.setState({ die1: newDie1, die2: newDie2, rolling: true });
+    setTimeout(() => {
+      this.setState({ rolling: false });
+    }, 1000);
   }
   render() {
     return (
       <div className="RollDice">
-        <Die face={this.state.die1} />
-        <Die face={this.state.die2} />
+        <div className="RollDice-container">
+          <Die face={this.state.die1} rolling={this.state.rolling} />
+          <Die face={this.state.die2} rolling={this.state.rolling} />
+        </div>
         <div>
-          <button onClick={this.roll}>Roll Dice!</button>
+          <button onClick={this.roll} disabled={this.state.rolling}>
+            {this.state.rolling ? "Rolling..." : "Roll Dice!"}
+          </button>
         </div>
       </div>
     );
